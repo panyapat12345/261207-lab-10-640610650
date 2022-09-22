a99ff80aa938bd1db494d54dd9af55ae7010f937
Panyapat Wongdee <panyapat12345@gmail.com>
2022-09-22 14:52:59 +0700

    ans1

--- pages/api/room.js
+++ pages/api/room.js
@@ -1,5 +1,16 @@
 import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
-  const rooms = readDB();
+  if (req.method === "GET") {
+    const rooms = readDB();
+    const roomData = [];
+    rooms.map((room) => {
+      const data = {
+        roomId: room.roomId,
+        roomName: room.roomName,
+      };
+      roomData.push(data);
+    });
+    res.json({ ok: true, rooms: roomData });
+  }
 }
--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,13 +4,32 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const roomId = req.query.roomId;
+    const idx = rooms.findIndex((room) => room.roomId === roomId);
+    if (idx === -1) {
+      res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      res.json({ ok: true, messages: rooms[idx].messages });
+    }
   } else if (req.method === "POST") {
     const rooms = readDB();
+    const roomId = req.query.roomId;
+    const idx = rooms.findIndex((room) => room.roomId === roomId);
 
-    //read request body
-    const text = req.body.text;
-
-    //create new id
-    const newId = uuidv4();
+    if (idx === -1) {
+      //not found
+      res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      if (typeof req.body.text === "string") {
+        // correct body
+        const newMessage = { messageId: uuidv4(), text: req.body.text };
+        rooms[idx].messages.push(newMessage);
+        writeDB(rooms);
+        res.json({ ok: true, message: newMessage });
+      } else {
+        // incorrect body
+        res.status(400).json({ ok: false, message: "Invalid text input" });
+      }
+    }
   }
 }
